73968efaa08d26dc63e79c7f990e41150431eb78
UFO <svobom57@gmail.com>
2014-11-28 00:42:52 +0100

    Added method first_operator_index

--- lib/abstract_syntax_tree_builder.rb
+++ lib/abstract_syntax_tree_builder.rb
@@ -5,16 +5,8 @@ class AbstractSyntaxTreeBuilder
 	end
 
 	def build_tree
-
 		until @postfix.length == 1
-			operator_index = @postfix.find_index { |token|
-				begin
-					Operator.factory!(token)
-					true
-				rescue
-					false
-				end
-			}
+			operator_index = first_operator_index
 			raise 'Not enough operators' if operator_index.nil?
 			exp = Operator.factory!(@postfix.slice!(operator_index))
 			most_left_child = operator_index - exp::ARITY
@@ -23,6 +15,18 @@ class AbstractSyntaxTreeBuilder
 			@postfix.insert(most_left_child, exp.new(*children))
 		end
 		@postfix.first
+	end
+
+	private
+	def first_operator_index
+		@postfix.find_index { |token|
+			begin
+				Operator.factory!(token)
+				true
+			rescue
+				false
+			end
+		}
 	end
 
 end
