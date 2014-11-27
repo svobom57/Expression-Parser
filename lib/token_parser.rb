a373c0fa33b07c795aff439d6b082196e125c3c9
Miroslav Csonka <miroslav.csonka@gmail.com>
2014-11-27 17:39:45 +0100

    Basic token parser implementation

--- lib/token_parser.rb
+++ lib/token_parser.rb
@@ -1,7 +1,7 @@
 class TokenParser
 
   def parse(input)
-    []
+    input.scan(/(>=)|([a-z]+)|([>])|([0-9]+)|([=])+|([(])+|([)])|([<])|([&]+)+|([|]+)+/).flatten.compact
   end
 
 end
--- spec/lib/token_parser_spec.rb
+++ spec/lib/token_parser_spec.rb
@@ -3,10 +3,28 @@ require './spec/spec_helper'
 describe 'Simple parsing' do
   let(:parser) { TokenParser.new }
 
-  it 'empty input' do
-    parsed = parser.parse('')
-
-    expect(parsed).to be_empty
-    expect(parsed.length).to eq 0
+  def returns(string, tokens)
+    expect(parser.parse(string)).to eq tokens
   end
+
+  it 'empty input' do
+    returns '', []
+  end
+
+  it 'single token' do
+    returns 'attribute', %w(attribute)
+  end
+
+  it 'simple assertions' do
+    returns 'weight > 0', %w(weight > 0)
+  end
+
+  it 'simple OR operator' do
+    returns '    true     || false', %w(true || false)
+  end
+
+  it 'simple AND operator' do
+    returns 'true && false', %w(true && false)
+  end
+
 end
