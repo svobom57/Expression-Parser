269bd16cc0556455f7fd2d915b19e6757609ebdd
UFO <svobom57@gmail.com>
2014-11-28 01:15:18 +0100

    Changed keys from :precedence! to :precedence

--- lib/Expressions/operator.rb
+++ lib/Expressions/operator.rb
@@ -12,10 +12,10 @@ class Operator
 
 	def self.precedence!(operator_symbol)
 		raise("Non-existent operator #{operator_symbol}") unless OPERATORS.key?(operator_symbol)
-		OPERATORS[operator_symbol][:precedence!]
+		OPERATORS[operator_symbol][:precedence]
 	end
 
 	def self.register(operator_symbol, class_name, precedence)
-		OPERATORS[operator_symbol] = {class_name: class_name, precedence!: precedence}
+		OPERATORS[operator_symbol] = {class_name: class_name, precedence: precedence}
 	end
 end
