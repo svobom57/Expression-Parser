bbaa4f88d1474f81056ebae11578a72eaf9c9cc7
Miroslav Csonka <miroslav.csonka@gmail.com>
2014-12-01 06:04:19 +0100

    Implemented generation of SQL for And operator

--- spec/lib/Expressions/operators_spec.rb
+++ spec/lib/Expressions/operators_spec.rb
@@ -13,12 +13,16 @@ describe Operator do
   end
 
   describe Equal do
-
     it 'simple case' do
       expect(Equal.new('price', '180').to_sql).to eq '(price = 180)'
     end
-
   end
 
+  describe And do
+    it 'Nested case' do
+      expression = And.new(Equal.new('price', UnaryMinus.new('180')), Equal.new('Group', 'Tabák'))
+      expect(expression.to_sql).to eq '((price = -180) AND (Group = Tabák))'
+    end
+  end
 
 end
